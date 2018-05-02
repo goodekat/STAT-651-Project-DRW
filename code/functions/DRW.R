e6e8adc13e947f94f5bed1c3d90f7eb4ae2960e6
Shan Yu <syu@iastate.edu>
2018-05-02 15:39:11 -0500

    Generate functions for DRW bootstrap

--- code/functions/DRW.R
+++ code/functions/DRW.R
@@ -1,28 +1,27 @@
+## STAT 651 FINAL PROJECT - DEPENDENT RANDOM WEIGHTING
+## Function for generate bootstrap sample using DRW
+
+# library needed library
 library('MASS')
-#### Generate data ####
-source('gents.R')
 
-X=genAR(100,0.5,sigma=1)
-n=length(X)
-#### Generate W ####
-#Z
-l=5
-I=matrix(rep(1:n,each=n),ncol=n)
-J=matrix(rep(1:n,n),ncol=n)
-D=(I-J)/l
-# should use bartlett window?
-K=exp(-D^2)
-
-
-#### One bootstrap sample ####
-# X is the time series
-# W is the W(t) is the Kernel value
-generate_bootstrap=function(X,K){
+DRW_bootstrap=function(X,l=5){
+  # sample size
   n=length(X)
   
+  # calucate (i-j)/d
+  I=matrix(rep(1:n,n),ncol=n)
+  J=matrix(rep(1:n,each=n),ncol=n)
+  D=(I-J)/l
+  
+  # evaluate using bartlett window function
+  K=1-abs(D)
+  K[K<0]=0
+  
+  # generate Y(t), Z(t) and W(t) in the paper
   Y=mvrnorm(n=1,mu=rep(0,n),Sigma=K)
   Z=(Y+sqrt(1+sqrt(2)))^2
   W=Z/sum(Z)
   
+  # generate bootstrap sample with weighted probability
   sample(X,replace=TRUE,prob=W)
 }
